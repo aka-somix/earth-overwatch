80ea6fa89f9a4a9643a6373e405c3459aac69ce8
Salvatore Cirone <s.cirone@reply.it>
2025-01-21 14:51:37 +0100

    :fire: (mlpipe) detach dataset management
    Moved into another component (DACOMS)

--- mlpipe/stacks/dataset-upload/_outputs.tf
+++
@@ -1,3 +0,0 @@
-output "dataset_uploader_invoke_arn" {
-  value = aws_lambda_function.dataset_uploader_service.invoke_arn
-}
--- mlpipe/stacks/sagemaker/_outputs.tf
+++ mlpipe/stacks/sagemaker/_outputs.tf
@@ -7,7 +7,3 @@ output "sagemaker_execution_role" {
 output "aws_s3_bucket_aimodels" {
   value = data.aws_s3_bucket.aimodelsbucket
 }
-
-output "datasets_efs_access_point" {
-  value = aws_efs_access_point.dataset_storage
-}
