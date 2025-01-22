f454a8d39a72e7c303dbb0a69ba17035c91d961b
Salvatore Cirone <s.cirone@reply.it>
2025-01-22 14:49:43 +0100

    :sparkles: (mlpipe/landfill) re-add build script for algorithm

--- mlpipe/stacks/landfill/main.tf
+++ mlpipe/stacks/landfill/main.tf
@@ -9,7 +9,7 @@ resource "aws_sagemaker_model" "yologeneric" {
   primary_container {
     image          = "763104351884.dkr.ecr.eu-west-1.amazonaws.com/pytorch-inference:2.4-cpu-py311"
     mode           = "SingleModel"
-    model_data_url = "s3://${var.aws_s3_bucket_aimodels.bucket}/models/genericyolo/model-v20241103.tar.gz"
+    model_data_url = "s3://${var.aws_s3_bucket_aimodels.bucket}/models/genericyolo/20250122/model.tar.gz"
   }
 }
 
