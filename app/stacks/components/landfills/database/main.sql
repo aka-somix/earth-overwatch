bae81fedf84812e83e1009cb43261dfb8ab4142b
Salvatore Cirone <s.cirone@reply.it>
2025-02-20 15:47:24 +0100

    :card_file_box: (components/landfills) update db and specification to …

--- app/stacks/components/landfills/database/main.sql
+++ app/stacks/components/landfills/database/main.sql
@@ -3,8 +3,10 @@ CREATE EXTENSION postgis;
 CREATE TABLE landfills (
     id 					SERIAL PRIMARY KEY,
     id_municipality     INT NOT NULL,
-    source              VARCHAR(20),                            -- SAT | SIG
+    source              VARCHAR(20),                            -- Source Product (exmaple: aerial)
     detection_time      TIMESTAMP,
+    confidence          SMALLINT                                -- normalized confidence * 100 (0,9 --> 90)
+    status              CHAR(1) ,                               -- V(alid) | I(nvalid) | U(nknown)
     area                GEOMETRY(MultiPolygon, 4326),           -- Using SRID 4326 for GPS coordinates (WGS84) 
     point_location      GEOMETRY(Point, 4326),                  -- Using SRID 4326 for GPS coordinates (WGS84) 
     created_at 			TIMESTAMP default CURRENT_TIMESTAMP,
